f7dad6ce5d347cb4da74b3baaa21807c16b5b423
LordCode <9999269010dddd@gmail.com>
2025-06-02 21:40:00 +0300

    up

--- test.py
+++ test.py
@@ -2,15 +2,18 @@ from fastapi.testclient import TestClient
 from main import app
 
 
-def test_read_items():
+def test():
     with TestClient(app) as client:
         response = client.get("/")
-        assert response.status_code == 404
+        assert response.status_code == 404, ('Receive %s status' %
+                                             response.status_code)
 
         response = client.get("/competitions/nearests")
-        assert response.status_code == 200
-        assert isinstance(response.json(), list)
+        assert response.status_code == 200, ('Receive %s status' %
+                                             response.status_code)
+        assert isinstance(response.json(), list), 'Receive %s type' % type(
+            response.json())
 
 
 if __name__ == '__main__':
-    test_read_items()
+    test()
