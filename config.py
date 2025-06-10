e158fb5f8754e313e9e6cd31e74266ae01fd4d21
LordCode <9999269010dddd@gmail.com>
2025-06-11 00:01:21 +0300

    update

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
+REDIS_URL = os.getenv("REDIS_URL")
 DATABASE_URL = os.getenv(
     "DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
