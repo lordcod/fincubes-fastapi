ddbccfbb8f12bc386079aafc64241cfb340e109c
LordCode <9999269010dddd@gmail.com>
2025-08-07 22:42:37 +0300

    stash

--- app/pages/me/auth/session/refresh/route.py
+++ app/pages/me/auth/session/refresh/route.py
@@ -1,8 +1,10 @@
 from datetime import timedelta
 from urllib.parse import urlparse
 from fastapi import APIRouter, Body, Depends, Request, Response
+import jwt
 from jwtifypy import JWTManager
 
+from app.core.errors import APIError, ErrorCode
 from app.core.security.deps.refresh_auth import RefreshTokenSecurity
 from app.models.user.user import User
 from app.schemas.auth.auth import TokenResponse
@@ -10,12 +12,35 @@ from app.schemas.auth.auth import TokenResponse
 router = APIRouter()
 EXPIRES_IN = int(timedelta(minutes=15).total_seconds())
 
+# ? UTILS FROM AUTH SECIRUTY
+
+
+async def get_token(request: Request) -> str:
+    token = request.cookies.get("refresh_token")
+    if not token:
+        raise APIError(ErrorCode.INVALID_TOKEN)
+    return token
+
+
+def decode_token(token: str) -> dict:
+    try:
+        payload = JWTManager.decode_token(token)
+    except jwt.ExpiredSignatureError as exc:
+        raise APIError(ErrorCode.EXPIRED_TOKEN) from exc
+    except jwt.PyJWTError as exc:
+        raise APIError(ErrorCode.INVALID_TOKEN) from exc
+    return payload
+
 
 @router.post("/", response_model=TokenResponse)
 async def refresh(
     request: Request,
     current_user: User = Depends(RefreshTokenSecurity()),
 ):
+    token = request.cookies.get("refresh_token")
+    if not token:
+        raise APIError(ErrorCode.INVALID_TOKEN)
+
     manager = JWTManager.with_issuer(request.url.path).with_audience("auth")
     access_token = manager.create_access_token(current_user.id, fresh=False)
 
