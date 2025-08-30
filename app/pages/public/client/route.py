9d32c95ffe08fe1364bce0dd884d2a027ce412fd
LordCode <9999269010dddd@gmail.com>
2025-08-30 22:37:58 +0300

    chore(protection): remove public/client URI from protection

--- app/pages/public/client/route.py
+++ app/pages/public/client/route.py
@@ -1,4 +1,4 @@
 from fastapi import APIRouter, Depends
 from app.core.protection.secure_request import SecureRequest
 
-router = APIRouter(dependencies=[Depends(SecureRequest())])
+router = APIRouter()
