0afc34172bc50700c0999aae2e170029d6efb367
LordCode <9999269010dddd@gmail.com>
2025-09-10 23:18:04 +0300

    upd

--- app/pages/me/role/athlete/avatar/route.py
+++ app/pages/me/role/athlete/avatar/route.py
@@ -1,8 +1,11 @@
+import asyncio
+import hashlib
 from fastapi import APIRouter, Depends, File, UploadFile
 
 from app.core.deps.ratelimit import create_ratelimit
 from app.core.errors import APIError, ErrorCode
-from app.integrations.yandexcloud import delete_file, upload_file
+from app.integrations.yandex_cdn_api import update_cdn_cache
+from app.integrations.yandexcloud import delete_file, extract_object_path, upload_file
 from app.models.athlete.athlete import Athlete
 from app.schemas.athlete.athlete import Athlete_Pydantic
 from app.shared.enums.enums import UserRoleEnum
@@ -11,7 +14,7 @@ from app.shared.utils.user_role import get_role
 router = APIRouter()
 
 MAX_SIZE = 16 * 1024 * 1024
-UPLOAD_INTERVAL_SECONDS = 60 * 60  # TODO REQUIRED CHANGE
+UPLOAD_INTERVAL_SECONDS = 60 * 60
 
 
 async def get_content(file: UploadFile):
@@ -35,8 +38,13 @@ async def upload_avatar(
 
     content = await get_content(file)
     ext = file.filename.split(".")[-1]
+    hash = hashlib.sha256(content).hexdigest()
 
-    avatar_url = await upload_file(content, f"{athlete.id}.{ext}")
+    filename = f"avatar/{athlete.id}_{hash[:16]}.{ext}"
+    avatar_url, _ = await asyncio.gather(
+        upload_file(content, filename),
+        update_cdn_cache(filename, action='prefetch')
+    )
     athlete.avatar_url = avatar_url
     await athlete.save()
 
