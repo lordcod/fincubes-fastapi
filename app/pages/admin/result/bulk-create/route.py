e440ab2ef996b4161df3382ea91299caf3c0b287
LordCode <9999269010dddd@gmail.com>
2025-10-02 12:25:32 +0300

    fix(bulk-results): update endpoint bulk-results, fix creating results (delete dsq/dsq_final)

--- app/pages/admin/result/bulk-create/route.py
+++ app/pages/admin/result/bulk-create/route.py
@@ -55,32 +55,23 @@ async def bulk_create_results(
                 db_result = Result(
                     athlete=athlete,
                     competition=competition,
-                    stroke=result.stroke,
-                    distance=result.distance,
-                    result=result.result,
-                    final=result.final,
-                    place=result.place,
-                    points=result.points,
-                    record=result.record,
-                    final_rank=result.final_rank,
-                    dsq=result.dsq,
-                    dsq_final=result.dsq_final,
+                    **result.model_dump()
                 )
 
                 if db_result.result:
                     await get_rank(
                         redis,
                         athlete.gender,
-                        result.stroke,
-                        result.distance,
+                        db_result.stroke,
+                        db_result.distance,
                         db_result.result,
                     )
                 if db_result.final:
                     await get_rank(
                         redis,
                         athlete.gender,
-                        result.stroke,
-                        result.distance,
+                        db_result.stroke,
+                        db_result.distance,
                         db_result.final,
                     )
                 results.append(db_result)
