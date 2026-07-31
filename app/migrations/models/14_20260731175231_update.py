bd982d3abfab258af17563dd2435b76e4d5213e1
LordCode <9999269010dddd@gmail.com>
2026-07-31 17:53:05 +0300

    feat: Add migration script for athlete and location object schema updates

--- app/migrations/models/14_20260731175231_update.py
+++ app/migrations/models/14_20260731175231_update.py
@@ -6,9 +6,9 @@ async def upgrade(db: BaseDBAsyncClient) -> str:
         ALTER TABLE "athletes" ADD "location_object_id" UUID;
         ALTER TABLE "athletes" DROP COLUMN "city";
         ALTER TABLE "athletes" DROP COLUMN "club";
-        ALTER TABLE "location_objects" DROP COLUMN "region_id";
         ALTER TABLE "location_objects" DROP COLUMN "club_id";
         ALTER TABLE "location_objects" DROP COLUMN "city_id";
+        ALTER TABLE "location_objects" DROP COLUMN "region_id";
         DROP TABLE IF EXISTS "locations";
         ALTER TABLE "athletes" ADD CONSTRAINT "fk_athletes_location_725e26e7" FOREIGN KEY ("location_object_id") REFERENCES "location_objects" ("id") ON DELETE SET NULL;"""
 
@@ -19,6 +19,6 @@ async def downgrade(db: BaseDBAsyncClient) -> str:
         ALTER TABLE "athletes" ADD "city" VARCHAR(255);
         ALTER TABLE "athletes" ADD "club" VARCHAR(255);
         ALTER TABLE "athletes" DROP COLUMN "location_object_id";
-        ALTER TABLE "location_objects" ADD "region_id" UUID NOT NULL;
         ALTER TABLE "location_objects" ADD "club_id" UUID;
-        ALTER TABLE "location_objects" ADD "city_id" UUID;"""
+        ALTER TABLE "location_objects" ADD "city_id" UUID;
+        ALTER TABLE "location_objects" ADD "region_id" UUID NOT NULL;"""
